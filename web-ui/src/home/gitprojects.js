43dd23e1db51bdea9363ac7a6df217afd46a169f
PlayerG9 <g9yout@gmail.com>
2022-02-27 13:42:09 +0100

    displays now git-repos (needs some styling)

--- web-ui/src/home/gitprojects.js
+++ web-ui/src/home/gitprojects.js
@@ -2,7 +2,54 @@ import React from "react"
 
 
 export default class GitProjects extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            repos: []
+        }
+    }
+
+    componentDidMount(){
+        this.loadRepos()
+    }
+
     render(){
-        return <p>GitProjects</p>
+        return <div className="top-hr">
+            <h1>GitProjects</h1>
+            <div className="grid">
+                {this.state.repos.map((repo, key) => <Repo key={key} data={repo} />)}
+            </div>
+        </div>
+    }
+
+    loadRepos(){
+        fetch('https://api.github.com/users/PlayerG9/repos')
+            .then((response) => response.json())
+            .then((data) => {
+                this.setState({
+                    repos: data
+                })
+            })
+    }
+}
+
+
+class Repo extends React.Component {
+    render(){
+        const repo = this.props.data
+        return <div className="repo">
+            <h1>{repo.name}</h1>
+            <p>{repo.description}</p>
+            <p>Created at: {this.formatDate(repo.created_at)}</p>
+            <p>Last Update: {this.formatDate(repo.updated_at)}</p>
+        </div>
+    }
+
+    formatDate(dateString){
+        var timestamp = Date.parse(dateString)
+        if(isNaN(timestamp))
+            return '~err~'
+        var date = new Date(timestamp)
+        return date.toLocaleDateString()
     }
 }
