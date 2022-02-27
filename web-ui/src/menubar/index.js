c486ef02ba1c449c0ddbfce0695a0e65859acb7b
PlayerG9 <g9yout@gmail.com>
2022-02-27 11:35:39 +0100

    created basic menubar/topnav

--- web-ui/src/menubar/index.js
+++ web-ui/src/menubar/index.js
@@ -5,9 +5,13 @@ import './index.css'
 export default class MenuBar extends React.Component {
     render(){
         return <div className="menubar">
-            <button>
-                Home
-            </button>
+            {/* this link should reload the page (equal to reload) */}
+            <a href=".">
+                <img src="logo512.png" alt="logo" />
+            </a>
+            <h1 className="no-select">
+                PlayerG9
+            </h1>
         </div>
     }
 }
