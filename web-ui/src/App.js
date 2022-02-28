10939512cb4ed1b5407f5116afecc8a34c301873
PlayerG9 <g9yout@gmail.com>
2022-02-28 17:40:44 +0100

    imported new icon.css

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'  // contains styles for the app
+import './icons.css' // contains icon-class to display small icon to the left of the element
 import './generell.css'  // contains generell styles (ex. for tables)
 import './ui_components/index.css'  // contains styles for ui-components like <Loader>
 import MenuBar from './menubar'
