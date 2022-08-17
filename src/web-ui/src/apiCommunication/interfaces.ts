c656e7a37f1c3716938307c4139842cf156d79a5
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-08-17 19:59:02 +0200

    updated apiData interface

--- src/web-ui/src/apiCommunication/interfaces.ts
+++ src/web-ui/src/apiCommunication/interfaces.ts
@@ -4,8 +4,13 @@ export interface RepoData {
     name: string,
     full_name: string,
     owner: {
-        login: "PlayerG9" | string
+        login: "PlayerG9" | string,
+        avatar_url: string,
+        html_url: string
     },
     description: string | null,
-    homepage: string | null
+    homepage: string | null,
+    html_url: string,
+    language: string | null,
+    default_branch: string | "main"
 }
