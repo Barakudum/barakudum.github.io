24735d45eb5f8d0edd9bc4f7e44c45c70cb04db9
PlayerG9 <68517540+PlayerG9@users.noreply.github.com>
2022-08-19 19:38:36 +0200

    added a me-welcome-screen

--- src/web-ui/src/App/index.tsx
+++ src/web-ui/src/App/index.tsx
@@ -1,6 +1,7 @@
 import './style.scss'
 import '../components/style.scss'
 import RateLimitWarner from './sections/RateLimitWarner'
+import MeScreen from './sections/MeScreen'
 import Websites from './sections/Websites'
 import Skills from './sections/Skills'
 import GithubProjects from './sections/GithubProjects'
@@ -9,6 +10,7 @@ import GithubProjects from './sections/GithubProjects'
 export default function App() {
     return <div className='app'>
         <RateLimitWarner/>
+        <MeScreen/>
         <Websites/>
         <Skills/>
         <GithubProjects/>
---
+++ src/web-ui/src/App/sections/MeScreen/index.tsx
@@ -0,0 +1,17 @@
+import './style.scss'
+import PlayerG9  from '../../../assets/PlayerG9.png'
+
+
+export default function MeScreen(){
+    return <div className='me-screen parallax'>
+        <div className='text-pane'>
+            <div>
+                <h1>PlayerG9</h1>
+                <h2>A Programmer without life</h2>
+            </div>
+        </div>
+        <div className='icon-pane'>
+            <img className='icon' src={PlayerG9} alt=""/>
+        </div>
+    </div>
+}
